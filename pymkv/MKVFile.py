a05510d0cb99879a88438beb8772e8d82d18a2ad
Trix <93526043+trixoniisama@users.noreply.github.com>
2022-09-11 17:23:31 +0200

    Update MKVFile.py

--- pymkv/MKVFile.py
+++ pymkv/MKVFile.py
@@ -176,7 +176,7 @@ class MKVFile:
                 command.extend(['--track-name', str(track.track_id) + ':' + track.track_name])
             # audio/subs delay
             if track.delay!=0:
-                command.extend(['--delay', str(track.track_id) + ':' + self._delay])
+                command.extend(['--delay', str(track.track_id) + ':' + self.delay])
             if track.language_ietf is not None:
                 command.extend(['--language', str(track.track_id) + ':' + track.language_ietf])
             elif track.language is not None:
